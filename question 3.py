303960364f4ad785e0c6f12a20baef5ea5c14b03
Mustapha <21mshitakha.acc@lwetb.ie>
2025-09-04 09:34:55 +0100

    Add files via upload

---
+++ question 3.py
@@ -0,0 +1,12 @@
+'''
+Author: Mustapha
+Date: 3 September 2025
+Description: If, else, elif excercise
+'''
+
+num= input('Please enter a number between 10 and 20: ')
+num= float(num)
+if(num>=10) and (num<=20):
+    print('Thank you')
+else:
+    print('Incorrect value entered')
