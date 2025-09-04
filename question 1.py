35762145d1ead225d187d6eb0fb6b2e350a1aa10
Mustapha <21mshitakha.acc@lwetb.ie>
2025-09-04 09:25:22 +0100

    Add files via upload

---
+++ question 1.py
@@ -0,0 +1,14 @@
+'''
+Author: Mustapha
+Date: 3 September 2025
+Description: If, else, elif excercise
+'''
+
+num1= input('Please enter a number ')
+num2= input('Enter another number ')
+num1= float(num1)
+num2= float(num2)
+if(num1>num2):
+    print('Your numbers are: ', num2, 'and', num1)
+elif(num2>num1):
+    print('Your numbers are: ', num1, 'and', num2)
