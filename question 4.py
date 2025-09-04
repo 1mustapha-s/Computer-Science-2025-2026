8f078fbc55192df2709178a8ef81e052151cf70a
Mustapha <21mshitakha.acc@lwetb.ie>
2025-09-04 09:43:56 +0100

    Add files via upload

---
+++ question 4.py
@@ -0,0 +1,12 @@
+'''
+Author: Mustapha
+Date: 3 September 2025
+Description: If, else, elif excercise
+'''
+
+colour= input('What is your favourite colour? ')
+colour= str(colour)
+if(colour == 'RED') or (colour == 'Red') or (colour == 'red'):
+    print('I like red too')
+else:
+    print('i dislike', colour, ', I prefer red')
