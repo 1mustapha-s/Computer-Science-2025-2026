373e2dc198f64b4d77ec92037c950d5f4f701529
Mustapha <21mshitakha.acc@lwetb.ie>
2025-09-04 09:31:09 +0100

    Add files via upload

---
+++ question 2.py
@@ -0,0 +1,12 @@
+'''
+Author: Mustapha
+Date: 3 September 2025
+Description: If, else, elif excercise
+'''
+
+num= input('Enter a number less than 20: ')
+num= float(num)
+if(num<20):
+    print('Thank you')
+else:
+    print('Too high')
