66c8d7a23d34216feb460d29201619f0f75bd589
Mustapha <21mshitakha.acc@lwetb.ie>
2025-09-05 09:28:47 +0100

    Add files via upload

---
+++ Question 6.py
@@ -0,0 +1,23 @@
+'''
+Author: Mustapha
+Date: 5 September 2025
+Description: If, else, elif excercise
+'''
+
+age= input('What is your age? ')
+age= float(age)
+if(age>= 18):
+    print('You can vote')
+elif(age== 17):
+    print('You can learn to drive')
+elif(age== 16):
+    print('You can buy a lottery ticket')
+elif(age<16):
+    print('You can go trick or treating')
+else:
+    print('Invalid')
+    
+    
+
+
+
