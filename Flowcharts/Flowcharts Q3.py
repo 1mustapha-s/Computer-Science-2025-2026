ca70b7664d8d440bf78b55a5f60c387892d57ae5
Mustapha <21mshitakha.acc@lwetb.ie>
2025-09-19 09:30:41 +0100

    Add files via upload

---
+++ Flowcharts/Flowcharts Q3.py
@@ -0,0 +1,22 @@
+'''
+Author: Mustapha
+Date: 19 September 2025
+Description: flowcharts excercise
+'''
+
+#Formula: p*r*t
+p= input('Enter the amount of money: ')
+p= float(p)
+r= input('Enter interest rate: ')
+r= float(r)
+t= input('Enter the amount of time: ')
+t= float(t)
+
+print('The simple interest rate is:', p*r*t)
+
+
+
+
+
+
+
