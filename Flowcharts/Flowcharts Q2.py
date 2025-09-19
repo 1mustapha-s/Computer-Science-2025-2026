431d89340738abf20787ccbdc799f94923017703
Mustapha <21mshitakha.acc@lwetb.ie>
2025-09-19 09:19:25 +0100

    Add files via upload

---
+++ Flowcharts/Flowcharts Q2.py
@@ -0,0 +1,11 @@
+'''
+Author: Mustapha
+Date: 19 September 2025
+Description: flowcharts excercise
+'''
+
+radius= input('Enter the radius of the circle: ')
+radius= float(radius)
+
+print('The circumference is:', 2*3.14159265359*radius)
+print('The area is:', 3.14159265359*radius**2) 
