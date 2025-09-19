fe5b2f6f56c2576e776041888f08ad18f0c6c245
Mustapha <21mshitakha.acc@lwetb.ie>
2025-09-19 09:05:53 +0100

    Add files via upload

---
+++ Flowcharts Q1.py
@@ -0,0 +1,18 @@
+'''
+Author: Mustapha
+Date: 19 September 2025
+Description: flowcharts excercise
+'''
+
+length= input('Enter the length of the quadrilateral: ')
+length= float(length)
+width= input('Enter the width of the quadrilateral: ')
+width= float(width)
+
+print('The perimeter is:', length+length+width+width)
+print('The area is:', length*width) 
+
+
+
+
+
