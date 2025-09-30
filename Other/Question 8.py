0bf3405d3ac07bc6188e31f5af42aef846479a79
Mustapha <21mshitakha.acc@lwetb.ie>
2025-09-30 14:20:39 +0100

    Add files via upload

---
+++ Other/Question 8.py
@@ -0,0 +1,23 @@
+'''
+Author: Mustapha
+Date: 30 September 2025
+Description: If, else, elif excercise
+'''
+
+num= input('Enter 1, 2 or 3: ')
+num= int(num)
+if num==1:
+    print('Thank You')
+elif num==2:
+    print('Well Done')
+elif num==3:
+    print('Correct')
+else:
+    print('Error Message')
+
+
+
+
+
+
+
