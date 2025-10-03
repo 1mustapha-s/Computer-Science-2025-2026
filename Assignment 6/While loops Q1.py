4c8ecd9aadc7e8522f6b34eea60cfb0e13c47194
Mustapha <21mshitakha.acc@lwetb.ie>
2025-10-03 09:41:46 +0100

    Add files via upload

--- Assignment 6/While loops Q1.py
+++ Assignment 6/While loops Q1.py
@@ -1,1 +1,20 @@
+'''
+Author: Mustapha
+Date: 3 October 2025
+Description: While loops exercise
+'''
+num = 2
+total = 0
+count = 0
+
+while num !='':
+    num= input('Enter any number: ')
+    if num.isdigit():
+        total+= int(num)
+        count+= 1
+print('Average is: ', total/count)
+
+
+
+
 
