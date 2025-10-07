53454a932fd239f2f207ca3adf2c0c4ecfa5cbf6
Mustapha <21mshitakha.acc@lwetb.ie>
2025-10-07 14:18:49 +0100

    Add files via upload

---
+++ Assignment 6/While loops Q4.py
@@ -0,0 +1,25 @@
+'''
+Author: Mustapha
+Date: 7 October 2025
+Description: While loops exercise
+'''
+
+
+num= 1
+while num != '':
+    num= input('Enter any whole number: ')
+    if num.isdigit():
+        num= int(num)
+        for i in range (1,num):
+            if (i%5 != 0):
+                print(i)
+            
+
+
+
+
+
+
+
+
+
