f1d1e6f9d3d083c6cd9840df7d078a07fbd1e1ff
Mustapha <21mshitakha.acc@lwetb.ie>
2025-10-21 14:40:25 +0100

    Add files via upload

---
+++ Assignment 10 strings question 1.py
@@ -0,0 +1,63 @@
+'''
+Author: Mustapha
+Date: 21 October 2025
+Description: String operations
+'''
+
+
+string1= input('enter a word: ')
+string1_2= string1.upper()
+print(string1_2)
+
+string2= input('enter a word: ')
+string2_2= string2.lower()
+print(string2_2)
+
+string3= input('enter a word: ')
+string3_2= string3.count('a')
+print(string3_2)
+
+string4= input('enter a word: ')
+string4_2= string4.find('a')
+print(string4_2)
+
+string5= input('enter a word: ')
+string5_2= string5.replace('a','e')
+print(string5_2)
+
+string6= input('enter a word: ')
+string6_2= string6.islower()
+print(string6_2)
+
+string7= input('enter a word: ')
+string7_2= string7.isupper()
+print(string7_2)
+
+string8= input('enter a word: ')
+string8_2= string8.isalnum()
+print(string8_2)
+
+string9= input('enter a word: ')
+string9_2= string9.isalpha()
+print(string9_2)
+
+string10= input('enter a word: ')
+string10_2= string10.isdigit()
+print(string10_2)
+
+string11= input('enter a word: ')
+string11_2= string11.index()
+print(string11_2)
+
+string12= input('enter a word: ')
+string12_2= string12.isalnum()
+print(string12_2)
+
+string13= input('enter a word: ')
+string13_2= string13.isalnum()
+print(string13_2)
+
+
+
+
+
