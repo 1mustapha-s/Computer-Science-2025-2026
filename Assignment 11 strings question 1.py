9a83a4fa81028721ff819246a8d127b0b4d65c9c
Mustapha <21mshitakha.acc@lwetb.ie>
2025-10-23 09:45:28 +0100

    Add files via upload

---
+++ Assignment 11 strings question 1.py
@@ -0,0 +1,14 @@
+'''
+Author: Mustapha
+Date: 23 October 2025
+Description: A11 String operations Q1
+'''
+
+
+length= 0
+while length!='':
+    word= input('Enter a word: ')
+    if word.isalpha():
+        print(word[::-1])
+
+
