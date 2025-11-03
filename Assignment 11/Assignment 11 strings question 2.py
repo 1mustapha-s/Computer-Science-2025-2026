05540ebfacb311e9fd89c93708385525e262a969
Mustapha <21mshitakha.acc@lwetb.ie>
2025-11-03 12:11:35 +0000

    Add files via upload

---
+++ Assignment 11/Assignment 11 strings question 2.py
@@ -0,0 +1,16 @@
+'''
+Author: Mustapha
+Date: 24 October 2025
+Description: A11 String operations Q2
+'''
+
+
+word= input('Enter a word: ')
+palindrome= word[::-1]
+if word== palindrome:
+    print('That is a palindrome')
+elif word!= palindrome:
+    print('That is not a palindrome')
+
+
+
