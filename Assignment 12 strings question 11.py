dddf49a1e956acbec43fbb3b7a77992cde879ccc
Mustapha <21mshitakha.acc@lwetb.ie>
2025-11-06 09:45:35 +0000

    Add files via upload

---
+++ Assignment 12 strings question 11.py
@@ -0,0 +1,17 @@
+'''
+Author: Mustapha
+Date: 5 November 2025
+Description: A12 String operations Q11
+'''
+
+
+string= input('Enter a sentence: ')
+words= string.count(' ')
+total= words+1
+print(f'There are {total} words in this sentence')
+
+
+
+
+
+
